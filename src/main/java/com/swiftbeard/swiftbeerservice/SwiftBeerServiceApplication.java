a17d749b163e663a52b05875d241106f20bfaee5
Ed <edmccormic@gmail.com>
2020-03-13 19:45:29 -0400

    edit #2

--- src/main/java/com/swiftbeard/swiftbeerservice/SwiftBeerServiceApplication.java
+++ src/main/java/com/swiftbeard/swiftbeerservice/SwiftBeerServiceApplication.java
@@ -3,8 +3,6 @@ package com.swiftbeard.swiftbeerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-//string test
 @SpringBootApplication
 public class SwiftBeerServiceApplication {
 
