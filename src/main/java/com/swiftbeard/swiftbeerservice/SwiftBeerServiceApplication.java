985a2b29874222ac4b91d0a8f207ef44aba25179
Ed <edmccormic@gmail.com>
2020-03-13 19:43:05 -0400

    adding web data model. closes #1

--- src/main/java/com/swiftbeard/swiftbeerservice/SwiftBeerServiceApplication.java
+++ src/main/java/com/swiftbeard/swiftbeerservice/SwiftBeerServiceApplication.java
@@ -3,6 +3,8 @@ package com.swiftbeard.swiftbeerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//string test
 @SpringBootApplication
 public class SwiftBeerServiceApplication {
 
